d235939d156bbca5e69d1cef04f791909d75df4f
Timofey Kirillov <timofey.kirillov@flant.com>
2019-10-02 00:31:03 +0300

    optimize

--- scripts/stapel/publish.sh
+++ scripts/stapel/publish.sh
@@ -4,5 +4,5 @@ set -e
 
 source scripts/stapel/version.sh
 
-docker push flant/werf-stapel-base:$CURRENT_STAPEL_VERSION
+#docker push flant/werf-stapel-base:$CURRENT_STAPEL_VERSION
 docker push flant/werf-stapel:$CURRENT_STAPEL_VERSION
